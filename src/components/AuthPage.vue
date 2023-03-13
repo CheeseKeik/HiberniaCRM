679c9e5e6e7e675155bfbd00cfd4b71382d1a835
AipNooBest <47401054+aipnoobest@users.noreply.github.com>
2023-03-13 17:45:33 +0300

    Это никто не должен видеть :)

--- src/components/AuthPage.vue
+++ src/components/AuthPage.vue
@@ -28,8 +28,8 @@ export default {
   name: "auth-page",
   data() {
     return {
-      username : 'danilov',
-      password : 'pupil',
+      username : '',
+      password : '',
       errors: '',
       error : false,
     };
